8358dad04d335ed398afdbe983ef0e49783072a5
inshallah123 <yangyihua06@gmail.com>
2025-08-06 09:27:13 +0800

    25-08-06 scheme finish

--- Project/scheme/questions.scm
+++ Project/scheme/questions.scm
@@ -6,19 +6,32 @@
 ;; Problem 15
 ;; Returns a list of two-element lists
 (define (enumerate s)
-  ; BEGIN PROBLEM 15
-  'replace-this-line
-  )
+  (define (helper lst index)
+    (if (null? lst)
+        '()
+        (cons (list index (car lst))
+              (helper (cdr lst) (+ index 1)))))
+  (helper s 0))
   ; END PROBLEM 15
 
 ;; Problem 16
 
 ;; Merge two lists S1 and S2 according to ORDERED? and return
 ;; the merged lists.
-(define (merge ordered? s1 s2)
-  ; BEGIN PROBLEM 16
-  'replace-this-line
-  )
+(define (merge ordered? lst1 lst2)
+  (cond ((null? lst1) lst2) ; 基本情况1: 如果 lst1 为空, 返回 lst2
+        ((null? lst2) lst1) ; 基本情况2: 如果 lst2 为空, 返回 lst1
+        (else
+         (let ((head1 (car lst1))
+               (head2 (car lst2)))
+           (if (ordered? head1 head2)
+               ; 如果 head1 应排在 head2 前面，
+               ; 则将 head1 作为结果的第一个元素，
+               ; 然后递归地合并 lst1 的剩余部分和整个 lst2
+               (cons head1 (merge ordered? (cdr lst1) lst2))
+               ; 否则，将 head2 作为结果的第一个元素，
+               ; 然后递归地合并整个 lst1 和 lst2 的剩余部分
+               (cons head2 (merge ordered? lst1 (cdr lst2))))))))
   ; END PROBLEM 16
 
 ;; Optional Problem 2
