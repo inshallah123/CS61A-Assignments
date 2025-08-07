e2df5098aa2212bbb4eef25983e006b092f220bb
inshallah123 <yangyihua06@gmail.com>
2025-08-07 10:11:55 +0800

    25-08-07 hw09

--- HW/hw09/hw09.scm
+++ HW/hw09/hw09.scm
@@ -1,4 +1,13 @@
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+(define (curry-cook formals body)
+  ; Base Case: If there is only one formal argument left.
+  (if (= (length formals) 1)
+      ; Construct the innermost lambda: (lambda (arg) body)
+      `(lambda (,(car formals)) ,body)
+
+      ; Recursive Step: If there are multiple formals.
+      ; Take the first formal and wrap the curried version of the rest.
+      `(lambda (,(car formals))
+         ,(curry-cook (cdr formals) body))))
 
 (define (curry-consume curry args)
   'YOUR-CODE-HERE)
